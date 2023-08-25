6b1e6e7cf5b387cd166051516677bb719d542adf
LDrevych <105346496+LDrevych@users.noreply.github.com>
2023-08-25 12:07:08 +0300

    Add initial project

---
+++ Test-Task/Test-Task/Test_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_TaskApp.swift
+//  Test-Task
+//
+//  Created by ldrevych on 25.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Test_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
