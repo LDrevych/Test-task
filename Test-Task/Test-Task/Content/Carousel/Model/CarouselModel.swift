23e63428183508bd295dde8c605c27c9200cb159
LDrevych <105346496+LDrevych@users.noreply.github.com>
2023-08-25 12:23:38 +0300

    Add CarouselModel

---
+++ Test-Task/Test-Task/Content/Carousel/Model/CarouselModel.swift
@@ -0,0 +1,12 @@
+//
+//  CarouselModel.swift
+//  Test-Task
+//
+//  Created by ldrevych on 25.08.2023.
+//
+
+import Foundation
+
+struct CarouselModel: Identifiable {
+    let id: UUID
+}
---
+++ Test-Task/Test-Task/Content/ViewModel/ContentViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ContentViewModel.swift
+//  Test-Task
+//
+//  Created by ldrevych on 25.08.2023.
+//
+
+import Foundation
